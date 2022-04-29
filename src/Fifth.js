144ee10095463dc2240b5ff317d60213b2e64650
Alan Douglas <aland6209@gmail.com>
2022-04-29 19:39:47 +0100

    first commit of the day with fifth component

--- src/Fifth.js
+++ src/Fifth.js
@@ -3,9 +3,57 @@ function Fifth() {
     <div className="fifth_container">
       <div className="block-five">
         <h2>WHY TO CALL UYO HOME</h2>
-        <hr></hr>
+        <hr className="mb-5"></hr>
 
-        
+        <div className="fifth-grid">
+          <div className="fifth-grid-item">
+            <div>
+              <b>Safety & security</b>
+              <p>
+                Having earned it's place as one of the safest Cities in Nigeria,
+                Akwa-Ibom boasts of low crime rates and a friendly environment
+                where peace & communal living thrive.
+              </p>
+            </div>
+          </div>
+
+          <div className="fifth-grid-item">
+            <div>
+              <b>Diversity & Communal Harmony</b>
+              <p>
+                Composed of people with a variety of demographic characteristics
+                yet still maintaining a united ethnicity, the state consisting
+                of warm and inviting citizens, beacons as an amicable state
+                opening its arms to culturally & ethnically diverse residents.
+              </p>
+            </div>
+          </div>
+
+          <div className="fifth-grid-item">
+            <div>
+              <b>Smooth & easy Accessibility</b>
+              <p>
+                Home to one of Nigeria's fastest growing airlines (Ibom Air),
+                Akwa-Ibom enables easy access to the rest of Nigeria via air,
+                land and as just recently being introduced, sea.
+              </p>
+            </div>
+          </div>
+
+          <div className="fifth-grid-item">
+            <div>
+              <b>Rapid development tendencies</b>
+              <p>
+                The city of Uyo stands with rapid development tendencies and in
+                coming years, will reach it's ideal height of commercial
+                greatness. The state believes in her ability to create
+                flourishing communities while doing so. An investment in Uyo's
+                real estate market provides you unrivalled advantages in the
+                long run.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
