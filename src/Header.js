3b59d7bbce46659bc52d79f8e621c635154cd29c
Alan Douglas <aland6209@gmail.com>
2022-05-01 17:04:38 +0100

    update!!

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,23 @@
+// import { Nav } from "react-bootstrap";
+
 function Header() {
-    return (
-        <div>
-            <header className="main_header">
-                V.A Properties and Homes
-            </header>
-        </div>
-    );
-  }
-  
-  export default Header;
-  
+  return (
+    <div>
+      <header className="main_header">V.A Properties and Homes</header>
+
+      {/* <Nav defaultActiveKey="/home" as="ul">
+        <Nav.Item as="li">
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+      </Nav> */}
+    </div>
+  );
+}
+
+export default Header;
